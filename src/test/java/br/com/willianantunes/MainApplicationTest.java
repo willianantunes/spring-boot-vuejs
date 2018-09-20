6b7975fb171f194585bfe0e32e8268cacb3744e5
Willian Antunes <willian.lima.antunes@gmail.com>
2018-09-20 20:41:32 -0300

    Application and connection details and renamed test

--- src/test/java/br/com/willianantunes/MainApplicationTest.java
+++ src/test/java/br/com/willianantunes/MainApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MainApplicationTests {
+public class MainApplicationTest {
 
     @Test
     public void contextLoads() {
