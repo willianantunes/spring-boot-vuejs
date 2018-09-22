d937a916e101d8cbf162aec707f3ca19aead582b
Willian Antunes <willian.lima.antunes@gmail.com>
2018-09-22 15:55:52 -0300

    Other approach to configure Jackson and use of RFC 7807

--- src/main/java/br/com/willianantunes/conf/SeriaDeserConfiguration.java
+++ src/main/java/br/com/willianantunes/conf/SeriaDeserConfiguration.java
@@ -2,24 +2,48 @@ package br.com.willianantunes.conf;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @Configuration
 public class SeriaDeserConfiguration {
 
     @Bean
-    public ObjectMapper objectMapper() {
+    Jackson2ObjectMapperBuilder jacksonBuilder() {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.registerModule(new JavaTimeModule());
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        return builder;
+    }
 
-        return objectMapper;
+    @Bean
+    JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
+    }
+
+    @Bean
+    Jdk8Module jdk8TimeModule() {
+        return new Jdk8Module();
+    }
+
+    /**
+     * Module for serialization/deserialization of RFC 7807.
+     *
+     * @see <a href="https://zalando.github.io/problem">Know more about Problem</a>
+     */
+    @Bean
+    ProblemModule problemModule() {
+        return new ProblemModule();
+    }
+
+    @Bean
+    ConstraintViolationProblemModule constraintViolationProblemModule() {
+        return new ConstraintViolationProblemModule();
     }
 }
